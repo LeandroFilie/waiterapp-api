c0235bc7f6737224e6bcdf6eca6bb3c684b59e84
LeandroFilie <leandro.gfilie@gmail.com>
2023-02-20 12:32:55 -0300

    chore: created use cases for products

--- src/app/router.ts
+++ src/app/router.ts
@@ -1,25 +1,36 @@
+import path from 'node:path';
+
 import { Router } from 'express';
+import multer from 'multer';
 
 import { listCategories } from './useCases/categories/listCategories';
 import { createCategory } from './useCases/categories/createCategory';
+import { listProducts } from './useCases/products/listProducts';
+import { createProduct } from './useCases/products/createProduct';
+import { listProductsByCategories } from './useCases/categories/listProductsByCategories';
 
 export const router = Router();
+
+const upload = multer({
+  storage: multer.diskStorage({
+    destination(req, file, callback) {
+      callback(null, path.resolve(__dirname, '..', '..', 'uploads'));
+    },
+    filename(req, file, callback) {
+      callback(null, `${Date.now()}-${file.originalname}`);
+    }
+  })
+});
 
 router.get('/categories', listCategories);
 
 router.post('/categories', createCategory);
 
-router.get('/products', (req, res) => {
-  res.send('Ok');
-});
+router.get('/products', listProducts);
 
-router.post('/products', (req, res) => {
-  res.send('Ok');
-});
+router.post('/products', upload.single('image'), createProduct);
 
-router.get('/products/:categoryId/products', (req, res) => {
-  res.send('Ok');
-});
+router.get('/categories/:categoryId/products', listProductsByCategories);
 
 router.get('/orders', (req, res) => {
   res.send('Ok');
