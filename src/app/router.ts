f4d2b0d4a0fadaad42aa840e9382eb9be06ff931
LeandroFilie <leandro.gfilie@gmail.com>
2023-02-20 12:58:35 -0300

    chore: created use cases for orders

--- src/app/router.ts
+++ src/app/router.ts
@@ -8,6 +8,10 @@ import { createCategory } from './useCases/categories/createCategory';
 import { listProducts } from './useCases/products/listProducts';
 import { createProduct } from './useCases/products/createProduct';
 import { listProductsByCategories } from './useCases/categories/listProductsByCategories';
+import { listOrders } from './useCases/orders/listOrders';
+import { createorder } from './useCases/orders/createOrder';
+import { changeOrderStatus } from './useCases/orders/changeOrderStatus';
+import { cancelOrder } from './useCases/orders/cancelOrder';
 
 export const router = Router();
 
@@ -32,19 +36,11 @@ router.post('/products', upload.single('image'), createProduct);
 
 router.get('/categories/:categoryId/products', listProductsByCategories);
 
-router.get('/orders', (req, res) => {
-  res.send('Ok');
-});
+router.get('/orders', listOrders);
 
-router.post('/orders', (req, res) => {
-  res.send('Ok');
-});
+router.post('/orders', createorder);
 
-router.patch('/orders/:orderId', (req, res) => {
-  res.send('Ok');
-});
+router.patch('/orders/:orderId', changeOrderStatus);
 
-router.delete('/orders/:orderId', (req, res) => {
-  res.send('Ok');
-});
+router.delete('/orders/:orderId', cancelOrder);
 
